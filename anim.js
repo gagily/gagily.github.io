0c9095f9a34636e2f735648128c1d20961c6c1ee
Dragana <dragana_zivkovic@rockemail.com>
2019-05-29 15:37:13 +0200

    Initial commit

---
+++ anim.js
@@ -0,0 +1,39 @@
+// Hide header..
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar-scroll").style.top = "0";
+  } else if (prevScrollpos < 100) {
+  }
+  else {
+    document.getElementById("navbar-scroll").style.top = "-120px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// Scroll..
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
